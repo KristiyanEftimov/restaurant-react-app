cf2f635a7c30d823465fd9601f0122d1ad346dee
Kristiyan Eftimov <kriskata233@abv.bg>
2023-03-23 21:06:37 +0200

    Fixed the logo and added dropdown to the NavBar

--- restaurant-react-app/src/Layout/Header.js
+++ restaurant-react-app/src/Layout/Header.js
@@ -1,24 +1,30 @@
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import classes from "./Header.module.css";
-import rest from './RestaurantLogo.png';
+import logo from './RestaurantLogo.png';
 
 
-export default function Header(){
-return (<Navbar className={classes}  bg="white" expand="lg">
-  <Container>
-    <Navbar.Brand className={classes.brand} href="#home"><img src={rest} className={classes.rest}></img></Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="me-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#about">About</Nav.Link>
-        <Nav.Link href="#services">Services</Nav.Link>
-        <Nav.Link href="#contact">Contact</Nav.Link>
-      </Nav>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>);
+export default function Header() {
+  return (
+    <Navbar className={classes} bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand className={classes.brand} href="#home">
+          <img src={logo} className={classes.logo} alt="restaurant logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto" bg="light">
+            <Nav.Link href="#home">Начало</Nav.Link>
+            <Nav.Link href="#about">За нас</Nav.Link>
+            <NavDropdown title="Меню" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#menu-lunch">Обедно меню</NavDropdown.Item>
+              <NavDropdown.Item href="#menu-dinner">Основно меню</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#contact">Контакти</Nav.Link>
+            <Nav.Link href="#reservation">Резервация</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
-
-
