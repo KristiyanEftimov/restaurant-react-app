22cb425ce205893d0d85e65d1cba7f1239f52d49
Kristiyan Eftimov <kriskata233@abv.bg>
2023-04-11 19:34:09 +0300

    Implemented ContactsForm and start of Reservation page

--- restaurant-react-app/src/Layout/components/ContactsForm.js
+++ restaurant-react-app/src/Layout/components/ContactsForm.js
@@ -1,46 +1,78 @@
-import { Form, Button,Row,Col } from 'react-bootstrap';
+import { useState } from 'react';
+import { Form, Button, Row, Col } from 'react-bootstrap';
+import emailjs from '@emailjs/browser';
 import classes from '../css/ContactsForm.module.css';
 
 export default function ContactsForm() {
-    return (
-        <Form className={classes.Form}>
-          <Row>
-            <Col>
-              <Form.Group controlId="formBasicFirstName">
-                <Form.Label></Form.Label>
-                <Form.Control type="text" placeholder="Име *"  required />
-              </Form.Group>
-            </Col>
-            <Col>
-              <Form.Group controlId="formBasicLastName">
-                <Form.Label></Form.Label>
-                <Form.Control type="text" placeholder="Фамилия *" required />
-              </Form.Group>
-            </Col>
-          </Row>
-    
-          <Row>
-            <Col>
-              <Form.Group controlId="formBasicEmail">
-                <Form.Label></Form.Label>
-                <Form.Control type="email" placeholder="Email *" required />
-              </Form.Group>
-            </Col>
-            <Col>
-              <Form.Group controlId="formBasicPhone">
-                <Form.Label></Form.Label>
-                <Form.Control type="tel" placeholder="Телефон *" required />
-              </Form.Group>
-            </Col>
-          </Row>
-    
-          <Form.Group controlId="formBasicComments">
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [comments, setComments] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const templateParams = {
+      firstName,
+      lastName,
+      email,
+      phone,
+      comments,
+    };
+
+    emailjs.send('service_ajfs478', 'template_4tvrfgp', templateParams, 'FYMcx3OMEr_ndqfRD')
+      .then((response) => {
+        console.log('SUCCESS!', response.status, response.text);
+        setFirstName('');
+        setLastName('');
+        setEmail('');
+        setPhone('');
+        setComments('');
+      }, (error) => {
+        console.log('FAILED...', error);
+      });
+  }
+
+  return (
+    <Form className={classes.Form} onSubmit={handleSubmit}>
+      <Row>
+        <Col>
+          <Form.Group controlId="formBasicFirstName">
             <Form.Label></Form.Label>
-            <Form.Control as="textarea" rows={3} placeholder="Въведете вашите въпроси или коментари" required />
+            <Form.Control type="text" name="firstName" value={firstName} onChange={(event) => setFirstName(event.target.value)} placeholder="Име *" required />
           </Form.Group>
-          <Button variant="primary" type="submit">
-            Изпрати
-          </Button>
-        </Form>
-      );
-}
+        </Col>
+        <Col>
+          <Form.Group controlId="formBasicLastName">
+            <Form.Label></Form.Label>
+            <Form.Control type="text" name="lastName" value={lastName} onChange={(event) => setLastName(event.target.value)} placeholder="Фамилия *" required />
+          </Form.Group>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label></Form.Label>
+            <Form.Control type="email" name="email" value={email} onChange={(event) => setEmail(event.target.value)} placeholder="Email *" required />
+          </Form.Group>
+        </Col>
+        <Col>
+          <Form.Group controlId="formBasicPhone">
+            <Form.Label></Form.Label>
+            <Form.Control type="tel" name="phone" value={phone} onChange={(event) => setPhone(event.target.value)} placeholder="Телефон *" required />
+          </Form.Group>
+        </Col>
+      </Row>
+
+      <Form.Group controlId="formBasicComments">
+        <Form.Label></Form.Label>
+        <Form.Control as="textarea" name="comments" value={comments} onChange={(event) => setComments(event.target.value)} rows={3} placeholder="Въведете вашите въпроси или коментари" required />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Изпрати
+      </Button>
+    </Form>
+  );
+} 
--- restaurant-react-app/src/Pages/Reservation.js
+++ restaurant-react-app/src/Pages/Reservation.js
@@ -1,11 +1,12 @@
 import Header from '../Layout/components/Header';
 import Footer from '../Layout/components/Footer';
+import ReservationImage from '../Layout/components/ReservationImage';
 
 function Reservation() {
     return (
         <div>
             <Header/>
-         
+            <ReservationImage/>
             <Footer/>
         </div>
     );
